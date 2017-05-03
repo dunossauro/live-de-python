c89ffb452a4e0ee6ba102413f682f45bbb6fa832
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2017-05-02 23:20:47 -0300

    teste 1

--- Live2/testes.py
+++ Live2/testes.py
@@ -1,1 +1,18 @@
-assert True
+from unittest import TestCase, main
+
+
+class Calc:
+    def soma(self):
+        pass
+
+
+class Testes(TestCase):
+
+    calc = Calc
+
+    def test_soma(self):
+        self.assertEqual(self.calc.soma(2, 2), 4)
+
+
+if __name__ == '__main__':
+    main()
