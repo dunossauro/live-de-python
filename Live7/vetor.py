58e3e8e38f682ac62b16087c21d5b363f9a9910e
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2017-06-07 21:57:41 -0300

    Live7 atualização

--- Live7/vetor.py
+++ Live7/vetor.py
@@ -12,9 +12,20 @@ class vetor:
     def __mul__(self, n):
         return (self.x * n, self.y * n)
 
-# v1 = vetor(2, 3)
+    def __rmul__(self, n):
+        return (self.x * n, self.y * n)
+
+# 2 + 2 - > 2.__add__(2)
+# 2 * 2 - > 2.__mul__(2)
+
+# v1 * 3 -> v1.__mul__(3)
+# 3 * v1 -> 3.__mul__(v1) -> TypeError
+# 3 * v1 -> v1.__rmul__(3) -> (6, 9)
+
+
+v1 = vetor(2, 3)
 # v2 = vetor(2, 2)
 #
 # #v1 * 3
 # print(v1 + v2)
-# print(v1 * v2)
+print(3 * v1)
