3099d2948b7932faa7c92589cbddd453ece3fe1a
devblackpower <stefan.dilenon@gmail.com>
2020-10-26 21:18:11 -0300

    SQLAlchemy ORM

---
+++ codigo/Live139/exemplo_01.py
@@ -0,0 +1,38 @@
+from sqlalchemy import create_engine, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///test.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class Pessoa(Base):
+    __tablename__ = 'pessoas'
+
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+
+Base.metadata.create_all(engine)
+
+
+p1 = Pessoa(nome='Fausto')
+p2 = Pessoa(nome='Fabio')
+p3 = Pessoa(nome='Arnaldo')
+p4 = Pessoa(nome='Fernando')
+p5 = Pessoa(nome='Leonardo')
+
+
+session.add(p1)
+session.add_all([p2, p3, p4])
+
+# persiste tudo que está na sessão
+session.commit() 
+
+session.add(p5)
+
+# limpa a sessão
+session.flush() 
+
---
+++ codigo/Live139/exemplo_03.py
@@ -0,0 +1,46 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+engine = create_engine('sqlite:///test.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class Produto(Base):
+    __tablename__ = 'produtos'
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    pessoa_id = Column(Integer, ForeignKey('pessoas.id'))
+    pessoa = relationship('Pessoa')
+
+    def __repr__(self):
+        return f'Produto(nome={self.nome}, pessoa={self.pessoa})'
+
+class Pessoa(Base):
+    __tablename__ = 'pessoas'
+
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    idade = Column(Integer)
+    produtos = relationship(Produto, backref='pessoas')
+
+    def __repr__(self):
+        return f'Pessoa(id={self.id, }nome={self.nome}, idade={self.idade}, produtos={self.produtos})'    
+    
+Base.metadata.create_all(engine)
+
+p1 = Pessoa(nome='Eduardo', idade='27')
+pd1 = Produto(nome='Livro', pessoa=p1)
+pd2 = Produto(nome='CD', pessoa=p1)
+
+session.add_all([pd1, pd2, p1])
+
+session.commit() 
+
+from pprint import pprint
+
+pprint(session.query(Pessoa).all())
