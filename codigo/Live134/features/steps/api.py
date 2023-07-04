3ac2484f1f5e48e69b25aa8a3334de16dfcec0cd
Ricardo Emanuel <r200518@dac.unicamp.br>
2023-07-04 19:03:42 -0300

    Linha 9 e 30 estavam selecionando por index um elemento da lista utilizando uma string e nao int

--- codigo/Live134/features/steps/api.py
+++ codigo/Live134/features/steps/api.py
@@ -6,11 +6,11 @@ from httpx import get, post
 
 @given('que exista uma tarefa')
 def inserir_tarefa(context):
-    feature_table = context.table['0']
+    feature_tabela = context.table[0]
     tarefa = {}
-    tarefa['title'] = feature_table['nome']
-    tarefa['description'] = feature_table['descrição']
-    tarefa['done'] = feature_table['estado']
+    tarefa['title'] = feature_tabela['nome']
+    tarefa['description'] = feature_tabela['descrição']
+    tarefa['done'] = feature_tabela['estado']
 
     assert post(context.base_url, json=tarefa).status_code == 201
 
@@ -21,19 +21,19 @@ def get_minhas_tarefas(context, endpoint):
 
 
 @then('não devo ter nenhuma tarefa para fazer')
-def checando_se_não_tenho_nenhuma_tarefa(context):
+def checando_se_nao_tenho_nenhuma_tarefa(context):
     assert context.request.json() == []
 
 
 @then('devo ter a seguinte tarefa para fazer')
-def checar_se_tarefa_esta_para_ser_feita(context):
-    feature_table = context.table['0']
+def checar_se_a_tarefa_esta_para_ser_feita(context):
+    feature_tabela = context.table[0]
     tarefa = {}
-    tarefa['title'] = feature_table['nome']
-    tarefa['description'] = feature_table['descrição']
-    tarefa['done'] = literal_eval(feature_table['estado'])
+    tarefa['title'] = feature_tabela['nome']
+    tarefa['description'] = feature_tabela['descrição']
+    tarefa['done'] = literal_eval(feature_tabela['estado'])
     response = context.request.json()
     del response[0]['id']
 
-
-    assert tarefa in response, f'{response} {tarefa}'
+    assert response == [tarefa], f'{response}'
+    
